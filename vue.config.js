6f84756ab7df1c81c7e8948859ce97a9c711a816
zhuoyujing <zhuoyj@hopnetwork.com>
2018-10-17 19:14:01 +0800

    [add]fix dist cannot show

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+//const path = require('path')
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
@@ -5,12 +6,13 @@ module.exports = {
       extensions: ['.js', '.json', '.vue'],
     },
   },
-    build: {
-        // Template for index.html
-      //  index: path.resolve(__dirname, '../dist/index.html'),
-
-        // Paths
-      //  assetsRoot: path.resolve(__dirname, '../dist'),
-      //  assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+    // build: {
+    //     // Template for index.html
+    //     //  index: path.resolve(__dirname, '../dist/index.html'),
+    //
+    //     // Paths
+    //     //assetsRoot: path.resolve(__dirname, '../dist'),
+    //       assetsSubDirectory: 'WebForShow-Dota2-Legue/dist/',
+    //     assetsPublicPath: './',
+    // }
 };
